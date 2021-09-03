265031f6128d4c3d5d384778c472daed102ff68e
gp5dbo65 <daveotis65@gmail.com>
2021-09-03 17:36:34 -0400

    test upload

---
+++ Assignment9/src/main/java/com/coderscampus/Assignment9/service/FileService.java
@@ -0,0 +1,8 @@
+package com.coderscampus.Assignment9.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+} //end of FileService class
---
+++ Assignment9/src/main/java/com/coderscampus/Assignment9/web/RecipeController.java
@@ -0,0 +1,8 @@
+package com.coderscampus.Assignment9.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+} //end of RecipeController class
